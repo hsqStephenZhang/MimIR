b25f6aec885dc2dc763a74024d71623af5ac9dfb
hsqStephenZhang <stephenzhang666666@gmail.com>
2026-06-18 19:12:37 +0200

    feat(tensor): fix RTTI issues, register op_pattern phase, and fix tensor lit tests

--- src/mim/plug/tensor/tensor.cpp
+++ src/mim/plug/tensor/tensor.cpp
@@ -2,11 +2,13 @@
 
 #include "mim/plugin.h"
 
+#include "mim/plug/tensor/phase/conv_to_img2col.h"
 #include "mim/plug/tensor/phase/fuse.h"
 #include "mim/plug/tensor/phase/lower.h"
 #include "mim/plug/tensor/phase/lower_get_set.h"
 #include "mim/plug/tensor/phase/lower_map_reduce.h"
 #include "mim/plug/tensor/phase/lower_to_mem.h"
+#include "mim/plug/tensor/phase/op_pattern.h"
 
 using namespace mim;
 using namespace mim::plug;
@@ -14,6 +16,7 @@ using namespace mim::plug;
 namespace mim::plug::tensor {
 void reg_phases(Flags2Phases& phases) {
     Phase::hook<lower_tensor, phase::Lower>(phases);
+    Phase::hook<conv_to_img2col, phase::ConvToImg2Col>(phases);
     Phase::hook<lower_map_reduce, phase::LowerMapReduce>(phases);
     Phase::hook<lower_get_set, phase::LowerGetSet>(phases);
     Phase::hook<fuse_tensor, phase::Fuse>(phases);
